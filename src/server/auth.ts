f533974a72e58b35e3589169f33412144618e8ac
lmac0040 <lmac0040@student.monash.edu>
2023-09-13 07:37:30 +0000

    feature: basic authentication (#1)

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -5,7 +5,11 @@ import {
   type DefaultSession,
   type NextAuthOptions,
 } from "next-auth";
+
+// Providers
 import DiscordProvider from "next-auth/providers/discord";
+import GoogleProvider from "next-auth/providers/google";
+import EmailProvider from "next-auth/providers/email";
 
 import { env } from "~/env.mjs";
 import { prisma } from "~/server/db";
@@ -51,16 +55,16 @@ export const authOptions: NextAuthOptions = {
     DiscordProvider({
       clientId: env.DISCORD_CLIENT_ID,
       clientSecret: env.DISCORD_CLIENT_SECRET,
+      authorization: { params: { prompt: "none" } },
     }),
-    /**
-     * ...add more providers here.
-     *
-     * Most other providers require a bit more work than the Discord provider. For example, the
-     * GitHub provider requires you to add the `refresh_token_expires_in` field to the Account
-     * model. Refer to the NextAuth.js docs for the provider you want to use. Example:
-     *
-     * @see https://next-auth.js.org/providers/github
-     */
+    GoogleProvider({
+      clientId: env.GOOGLE_CLIENT_ID,
+      clientSecret: env.GOOGLE_CLIENT_SECRET,
+    }),
+    EmailProvider({
+      server: env.EMAIL_SERVER,
+      from: env.EMAIL_FROM,
+    }),
   ],
 };
 
